6274cb7248250e21816b5068a79ad6d0cd1a54dd
이지헌 <110290060+jhkr1@users.noreply.github.com>
2026-06-24 19:53:01 +0900

    Delete test directory
    Delete test directory

--- test/src/components/Button.js
+++
@@ -1,5 +0,0 @@
-export const Button = ({ children, onClick, disabled, type = "button" }) => (
-  <button type={type} onClick={onClick} disabled={disabled} className="btn">
-    {disabled ? '처리 중...' : children}
-  </button>
-);
